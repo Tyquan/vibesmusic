29f58f03f2c541cde33b375fb3ff152f60dcb59b
Tyquan <tyquanjreddick@gmail.com>
2019-01-16 23:25:40 -0500

    update

--- public/javascripts/videos/all.js
+++ public/javascripts/videos/all.js
@@ -3,7 +3,7 @@ $.get('/api/v1/videos', function(data){
 	var div = $(`<div>`);
 	div.addClass("row");
 	data.forEach(function (d) {
-		div.append(`<div id="col-sm-6"><img id="videoImages" src=${d.mainImage} /><h1 id="videoImageTitles"><a href="/showvideo/${d._id}">${d.title}</a</h1></div>`);
+		div.append(`<div id="col-sm-6"><a href="/showvideo/${d._id}"><img id="videoImages" src=${d.mainImage} /><h1 id="videoImageTitles">${d.title}</h1></a></div>`);
 	});
 
 	$("#allVideos").append(div);
