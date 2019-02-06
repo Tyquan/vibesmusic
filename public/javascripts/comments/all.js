a0377242ffff2fe31b8b427a265550737bf5d296
Tyquan <tyquanjreddick@gmail.com>
2019-02-06 13:47:54 -0500

    updated

--- public/javascripts/comments/all.js
+++ public/javascripts/comments/all.js
@@ -1,28 +1,31 @@
 // Getting the Latest Video Should be 3 returned
 $.get(`/api/v1/videos${window.location.pathname}`, function(data){
-
-	//console.log(data.comments);
-
 	var result = $(`<ul>`);
+	var noCommentsHtml = `<div id="good-hide"><br><h1 class="text-center">No Comments Added Yet</h1></div>`;
 
-	result.addClass('list-group');
+	addStyles(result);
+	renderComments(noCommentsHtml);
+	
 
-	result.addClass('comment-result');
-
-	if (data.comments.length < 1) {
-		result.append(`<div id="good-hide"><br><h1 class="text-center">No Comments Added Yet</h1></div>`);
-	} else {
-		//videoComments = data.comments.sort(function(a,b){ return a.date_created - b.date_created; });
-
-		//console.log(`Video Comments: ${videoComments}`);
-
-		data.comments.sort().forEach(function(comment) {
-			result.append(`<li class="list-group-item"><h5>${comment.name}</h5><pre id="userComments">${comment.body}</pre></li>`);
-		});
+	function addStyles (result) {
+		result.addClass('list-group');
+		result.addClass('comment-result');
 	}
 
-	
-	
-	$('#comments').append(result);
+	function renderComments (noCommentsHtml) {
+		if (data.comments.length < 1) {
+			result.append(noCommentsHtml);
+		} else {
+			data.comments.sort().forEach(function(comment) {
+				var commentsHtml = `
+					<li class="list-group-item">
+						<h5>${comment.name}</h5>
+						<pre id="userComments">${comment.body}</pre>
+					</li>`
+				result.append(commentsHtml);
+			});
+		}
+		$('#comments').append(result);
+	}
 
 });
--- public/javascripts/videos/show_video.js
+++ public/javascripts/videos/show_video.js
@@ -3,7 +3,32 @@ $.get(`/api/v1/videos${window.location.pathname}`, function(data){
 
 	console.log(data);
 
-	var result = `<h3 class="text-center" id="vidTitle">${data.title}</h3><br><iframe id="mainVideo" src=${data.link} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><hr><div class="row"><div class="col-sm-6"><h6>Posted: ${data.date_created}</h6><h6><span id="views">${data.views}</span> Views</h6></div><div class="col-sm-6"><a class="a2a_dd" href="https://www.addtoany.com/share"><img src="https://static.addtoany.com/buttons/share_save_171_16.png" width="171" height="16" border="0" alt="Share"></a><script async src="https://static.addtoany.com/menu/page.js"></script></div></div><br><div class="card"><div class="card-body"><pre id="videoDescription">${data.description}</pre></div></div>`;
+	var result = `
+		<h3 class="text-center" id="vidTitle">${data.title}</h3>
+		<br>
+		<iframe id="mainVideo" src=${data.link} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<hr>
+		<div class="row">
+			<div class="col-sm-6">
+				<h6>Posted: ${data.date_created}</h6>
+				<h6>
+					<span id="views">${data.views}</span> Views
+				</h6>
+			</div>
+			<div class="col-sm-6">
+				<a class="a2a_dd" href="https://www.addtoany.com/share">
+					<img src="https://static.addtoany.com/buttons/share_save_171_16.png" width="171" height="16" border="0" alt="Share">
+				</a>
+				<script async src="https://static.addtoany.com/menu/page.js"></script>
+			</div>
+		</div>
+		<br>
+		<div class="card">
+			<div class="card-body">
+				<pre id="videoDescription">${data.description}</pre>
+			</div>
+		</div>
+	`;
 	
 	$('#showVideo').append(result);
 
