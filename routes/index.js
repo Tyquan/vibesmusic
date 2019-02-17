538871ab76b989443800d0361a7360655730ae5b
Tyquan <tyquanjreddick@gmail.com>
2019-02-17 01:31:47 -0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -8,49 +8,49 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { 
-	    title: 'Herra HipHop | Hip Hop'
+	    title: 'Herra Hip Hop | Hip Hop'
 	});
 });
 
 router.get('/showvideo/:id', (req, res, next) => {
 	res.render('pages/showvideo', { 
-	    title: 'Herra HipHop | Watch Video'
+	    title: 'Herra Hip Hop | Watch Video'
 	});
 });
 
 router.get('/aboutus', (req, res, next) => {
 	res.render('pages/aboutus', { 
-	    title: 'Herra HipHop | About Us'
+	    title: 'Herra Hip Hop | About Us'
 	});
 });
 
 router.get('/music', (req, res, next) => {
 	res.render('pages/music', { 
-	    title: 'Herra HipHop | Music Videos'
+	    title: 'Herra Hip Hop | Music Videos'
 	});
 });
 
 router.get('/interviews', (req, res, next) => {
 	res.render('pages/interviews', { 
-	    title: 'Herra HipHop | Interviews'
+	    title: 'Herra Hip Hop | Interviews'
 	});
 });
 
 router.get('/news', (req, res, next) => {
 	res.render('pages/news', { 
-	    title: 'Herra HipHop | News'
+	    title: 'Herra Hip Hop | News'
 	});
 });
 
 router.get('/podcasts', (req, res, next) => {
 	res.render('pages/podcasts', { 
-	    title: 'Herra HipHop | Podcasts and Shows'
+	    title: 'Herra Hip Hop | Podcasts and Shows'
 	});
 });
 
 router.get('/contactus', (req, res, next) => {
 	res.render('forms/contactus', { 
-	    title: 'Herra HipHop | Contact Us'
+	    title: 'Herra Hip Hop | Contact Us'
 	});
 });
 
@@ -71,7 +71,7 @@ router.get('/sorrypage', (req, res, next) => {
 
 router.get('/submit', (req, res, next) => {
 	res.render('forms/submit', { 
-	    title: 'Herra HipHop | Submit a Video'
+	    title: 'Herra Hip Hop | Submit a Video'
 	});
 });
 
@@ -88,7 +88,7 @@ router.post('/submit', (req, res, next) => {
 
 router.get('/privacy', (req, res, next) => {
 	res.render('pages/privacy', { 
-	    title: 'Herra HipHop | Privacy Page'
+	    title: 'Herra Hip Hop | Privacy Page'
 	});
 });
 
