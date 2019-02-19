46efdf3e559e0b77952872f09fff95d686941b83
Tyquan <tyquanjreddick@gmail.com>
2019-02-19 16:02:00 -0500

    updated

--- public/javascripts/videos/latest_podcasts.js
+++ public/javascripts/videos/latest_podcasts.js
@@ -1,22 +1,47 @@
 // Getting the Latest Video Should be 1 returned
-$.get('/api/v1/videos/latest_podcasts', function(data){
+$.get('/api/v1/videos/', function(data){
 	
 	var div = $(`<div>`);
 	div.addClass("row");
+	var rows = [];
+	for (var i = 1; i < data.length; i++) {
+		if (rows.length <= 2) {
+			if (data[i].category == "Podcast" || data[i].category == "Show" || data[i].category == "Trailers" || data[i].category == "Announcements") {
+				rows.push(data[i]);
+			}
+		}
+	}
 
-	data.forEach(function (d) {
-
+	for (var i = 0; i < rows.length; i++) {
 		let allVideoHtml = `<div class="col-md-12">
-			<a href="/showvideo/${d._id}">
-				<img id="sideImages" src=${d.mainImage} />
-				<h1 id="videoImageTitles">${d.title}</h1>
+			<a href="/showvideo/${rows[i]._id}">
+				<img id="sideImages" src=${rows[i].mainImage} />
+				<h1 id="videoImageTitles">${rows[i].title}</h1>
 			</a>
 		</div>`;
-
 		div.append(allVideoHtml);
+	}
 
-	});
+	//console.log(data);
+	// data.forEach(function (d) {
+	// 	if (d.category == "Podcast" || d.category == "Show" || d.category == "Trailers" || d.category == "Announcements") {
+	// 		if (rows.length <= 3) {
+	// 			let allVideoHtml = `<div class="col-md-12">
+	// 				<a href="/showvideo/${d._id}">
+	// 					<img id="sideImages" src=${d.mainImage} />
+	// 					<h1 id="videoImageTitles">${d.title}</h1>
+	// 				</a>
+	// 			</div>`;
+	// 			rows.push(allVideoHtml);
+
+	// 			//div.append(allVideoHtml);
+	// 			div.append(rows);
+	// 			console.log(div);
+	// 		}
+	// 	}
 
 	$('#latest_podcasts').append(div);
-
 });
+
+	
+
--- public/javascripts/videos/latest_ten.js
+++ public/javascripts/videos/latest_ten.js
@@ -4,14 +4,30 @@ $.get('/api/v1/videos/latest_ten', function(data){
 	var div = $(`<div>`);
 	div.addClass("row");
 
+	let today = new Date();
+	let yesterday = today.toUTCString();
+	console.log(yesterday);
+	console.log(yesterday + 1);
+	let allVideoHtml = ``;
+
 	data.forEach(function (d) {
 
-		let allVideoHtml = `<div id="col-sm-6">
-			<a href="/showvideo/${d._id}">
-				<img id="videoImages" src=${d.mainImage} />
-				<h1 id="videoImageTitles">${d.title}</h1>
-			</a>
-		</div>`;
+		if (d.date_created == yesterday) {
+			allVideoHtml = `<div id="col-sm-6">
+				<a href="/showvideo/${d._id}">
+					<img id="videoImages" src=${d.mainImage} />
+					<h1 id="videoImageTitles">${d.title} (NEW)</h1>
+				</a>
+			</div>`;
+		} else {
+			allVideoHtml = `<div id="col-sm-6">
+				<a href="/showvideo/${d._id}">
+					<img id="videoImages" src=${d.mainImage} />
+					<h1 id="videoImageTitles">${d.title}</h1>
+				</a>
+			</div>`;
+		}
+		
 
 		div.append(allVideoHtml);
 
